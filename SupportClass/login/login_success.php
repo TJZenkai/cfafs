da94c1cb44206742eb21ad13c41dc4a90fb15391
flyingabove <flyingabove@gmail.com>
2014-10-08 17:20:28 -0700

    more small changes

--- SupportClass/login/login_success.php
+++ SupportClass/login/login_success.php
@@ -1,13 +1,23 @@
 <?php
-	if(!isset($_COOKIE['loggedin'])){
-	echo '<meta http-equiv="refresh" content="0; url=http://collegefafs.com/" />';
+	//if(!isset($_COOKIE['loggedin'])){
+	//echo '<meta http-equiv="refresh" content="0; url=http://www.collegefafs.com/" />';
 
-	}
+	//}
 ?>
-
+<!--
 <html>
 	<body>
 		<h1>Welcome!</h1>
 		<a href="logout.php">Logout</a>
+	</body>
+</html>
+
+-->
+
+
+<!-- teja I wrote this part cause i dont understand that fancy stuff glhf-->
+<html>
+	<body>
+		<meta http-equiv="refresh" content="0; url=http://www.collegefafs.com/" />
 	</body>
 </html>
